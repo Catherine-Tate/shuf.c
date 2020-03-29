da6379349d6d290602e159b5f92b65ce657019c4
Rylan <rylanoc@gmail.com>
2020-03-28 21:21:51 -0400

    add basic shuffling code

---
+++ util.c
@@ -0,0 +1,17 @@
+
+// randomly shuffle an array of lines
+// note: this will shuffle the array in place instead of returning a new copy
+void shuffle(char **lines, unsigned int num_lines) {
+  int random_index;
+  for (int index = 0; index < num_lines; ++index) {
+    random_index = arc4random_uniform(num_lines);
+    swap(lines, random_index, index);
+  }
+}
+
+// swap two elements in an array
+void swap(char **buf, unsigned int idx_new, unsigned int idx_old) {
+  char *tmp = buf[idx_old];
+  buf[idx_old] = buf[idx_new];
+  buf[idx_new] = tmp;
+}
